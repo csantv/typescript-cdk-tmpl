ac58b972a59e5bff629773cbdfd2418f412c3116
csantv <csantve@gmail.com>
2025-03-12 20:41:48 -0500

    add sample s3 bucket

--- lib/base.ts
+++ lib/base.ts
@@ -1,8 +1,20 @@
 import * as cdk from "aws-cdk-lib";
+import {aws_s3 as s3} from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { NagSuppressions } from "cdk-nag";
 
 export class BaseStack extends cdk.Stack {
+    bucket: s3.Bucket;
+
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        this.bucket = new s3.Bucket(this, "Bucket", {
+            enforceSSL: true
+        });
+
+        NagSuppressions.addResourceSuppressions(this.bucket, [
+            {id: "AwsSolutions-S1", reason: "Access Logs Not Needed"}
+        ]);
     }
 }
