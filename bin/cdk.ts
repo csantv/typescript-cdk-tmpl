098b62668f20c8131d425c849c03dde2cbfae5c0
csantv <csantve@gmail.com>
2025-03-12 20:46:50 -0500

    use cdk-nag on all stacks

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -5,13 +5,13 @@ import { AwsSolutionsChecks } from "cdk-nag";
 import { Aspects } from "aws-cdk-lib";
 
 const app = new cdk.App();
-const stack = new BaseStack(app, "BaseTask", {
+new BaseStack(app, "BaseTask", {
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEFAULT_REGION
     }
 });
 
-Aspects.of(stack).add(new AwsSolutionsChecks({
+Aspects.of(app).add(new AwsSolutionsChecks({
     verbose: true,
 }));
